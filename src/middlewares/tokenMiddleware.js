791938e27e631e62283bc3dc3641b20c1cca210a
NicolasNegrier <98546317+NicolasNegrier@users.noreply.github.com>
2023-06-22 16:00:20 +0200

    Update tokenMiddleware.js

--- src/middlewares/tokenMiddleware.js
+++ src/middlewares/tokenMiddleware.js
@@ -1,1 +1,16 @@
+require("dotenv").config();
 
+module.exports = (req, res, next) => {
+  try {
+    // Recupération du token envoyé par le client par dans les headers
+    const token = req.header("x-auth-token");
+    // Vérification du token
+    if (!token || token != process.env.TOKEN) {
+      return res.status(403).send("Access denied.");
+    } else {
+      next();
+    }
+  } catch (error) {
+    res.status(400).send("Invalid token");
+  }
+};
