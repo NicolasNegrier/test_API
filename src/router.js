32a5ac170697038a0ba38dc7395e01cc98b3749a
NicolasNegrier <98546317+NicolasNegrier@users.noreply.github.com>
2023-06-22 15:57:03 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,1 +1,21 @@
+// Importation des modules necessaires au fonctionnement de l'API
+const express = require('express');
 
+// Importation de notre controller
+const productsController = require('../src/controllers/productsController');
+
+// Initialisation du router
+const router = express.Router();
+
+// Route récupération de tous les produits
+router.get('/products', productsController.getProducts);
+// Route d'ajout d'un produit à la liste de produits
+router.post('/products', productsController.postProducts);
+// Route de récupération d'un produit par son id
+router.get('/products/:id', productsController.getOneProducts);
+// Route de modification d'un produit
+router.put('/products/:id', productsController.putProducts);
+// Route de suppression d'un produit
+router.delete('/products/:id', productsController.deleteProducts);
+
+module.exports = router;
